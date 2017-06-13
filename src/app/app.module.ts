854a0021672a84a5d5a92fed32c46246ef13a605
anubhavshrimal <anubhavshrimal@gmail.com>
2017-06-13 12:11:56 +0530

    Added routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { SideBannerComponent } from './components/side-banner/side-banner.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ChatPageComponent } from './components/chat-page/chat-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBannerComponent,
-    SignupComponent
+    SignupComponent,
+    ChatPageComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule,
