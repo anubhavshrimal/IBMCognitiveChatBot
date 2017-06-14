18565e372d572bd44d23d350e257db99c87fde6d
anubhavshrimal <anubhavshrimal@gmail.com>
2017-06-14 15:26:37 +0530

    Added input text field at the bottom of the chat page

--- src/app/components/chat-page/chat-window/chat-window.component.ts
+++ src/app/components/chat-page/chat-window/chat-window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'chat-window',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-window.component.css']
 })
 export class ChatWindowComponent implements OnInit {
+  @Input('show-profile') showUserProfile: boolean;
+  @Input('avatar-image') avatar: string;
+  @ViewChild('scrollElem') private myScrollContainer: ElementRef;
 
-  constructor() { }
+  public msg: string;
+  public strings: Array<string>;
+
+  constructor() {
+    this.showUserProfile = false;
+    this.msg = '';
+    this.strings = [];
+  }
 
   ngOnInit() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
+
+  add_msg() {
+    if (this.msg.trim().length != 0) {
+      this.strings.push(this.msg.trim());
+      this.msg = '';
+      setTimeout(() => { this.scrollToBottom(); });
+    }
   }
 
 }
